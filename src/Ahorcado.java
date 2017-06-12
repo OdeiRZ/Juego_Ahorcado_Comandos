d9f085abc7b339dc1487f80aeb6246af9e10033a
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2017-06-12 15:55:51 +0200

    Agregada lógica a clase ahorcado

--- src/Ahorcado.java
+++ src/Ahorcado.java
@@ -1,3 +1,103 @@
 import java.util.Scanner;
 import java.io.IOException;
 import java.util.Date;
+
+public class Ahorcado {
+    
+    public static void main(String[] args) throws IOException {
+        Scanner input=new Scanner(System.in);
+        String palabras[] = { "arlequin", "ambrosio",  "macgyver", "chiquetete",
+                              "torrente", "chanquete", "mortadelo","paquirrin",
+                              "espinete", "povedilla", "romualdo", "barcenas"};
+        String eleccion = palabras[(int)(Math.random()*palabras.length)]; 
+        String letrasUsadas = "", aux = "";
+        int comparar[] = new int[eleccion.length()], errores=0;
+        char caracter;	
+        boolean sw=false;
+
+        System.out.println("!!Juego del Ahorcado con Personajes¡¡\n");
+        for(int i=0;i<eleccion.length();i++)
+            aux=aux+"_ ";
+
+        long ti=new Date().getTime();
+        do {
+            System.out.print(aux+"\tErrores: "+errores);
+            if(letrasUsadas.length()>0) {
+                System.out.print("\tLetras usadas: ");
+                for(int i=0;i<letrasUsadas.length();i++)
+                    System.out.print(letrasUsadas.charAt(i)+", ");
+            }
+
+            System.out.print("\n\nIntroduce Letra: ");
+            caracter=input.next().charAt(0);
+                letrasUsadas+=caracter;
+
+                if(eleccion.indexOf(caracter)>=0) {
+                    aux="";sw=true;
+                    for(int i=0;i<comparar.length;i++) {
+                        if(caracter==eleccion.charAt(i))
+                                comparar[i]=1;
+                        if(comparar[i]==1)
+                                aux+=eleccion.charAt(i)+" ";
+                        else
+                                aux += "_ ";
+                    }
+                    for(int j=0;j<comparar.length;j++)
+                        if(comparar[j]!=1)
+                            sw=false;
+                    if(sw)
+                        System.out.println("\nFelicidades. Acertaste el Nombre :)");
+                } else {
+                        errores++;
+                        System.out.println();
+                        switch(errores) {
+                            case 1:	
+                                        System.out.println("\t\t\t   _ _");
+                            break;
+                            case 2:	System.out.println("\t\t\t    /");
+                                        System.out.println("\t\t\t    |");
+                                        System.out.println("\t\t\t    |");
+                                        System.out.println("\t\t\t   _|_");
+                            break;
+                            case 3:     System.out.println("\t\t\t    /¯¯¯¯¯");
+                                        System.out.println("\t\t\t    |");
+                                        System.out.println("\t\t\t    |");
+                                        System.out.println("\t\t\t   _|_");
+                            break;
+                            case 4:	System.out.println("\t\t\t    /¯¯¯¯¯|");
+                                        System.out.println("\t\t\t    |");
+                                        System.out.println("\t\t\t    |");
+                                        System.out.println("\t\t\t   _|_");
+                            break;
+                            case 5:     System.out.println("\t\t\t    /¯¯¯¯¯|");
+                                        System.out.println("\t\t\t    |     o");
+                                        System.out.println("\t\t\t    |");
+                                        System.out.println("\t\t\t   _|_");
+                            break;
+                            case 6:	System.out.println("\t\t\t    /¯¯¯¯¯|");
+                                        System.out.println("\t\t\t    |     o");
+                                        System.out.println("\t\t\t    |     |");
+                                        System.out.println("\t\t\t   _|_");
+                            break;
+                            case 7:	System.out.println("\t\t\t    /¯¯¯¯¯|");
+                                        System.out.println("\t\t\t    |    _o_");
+                                        System.out.println("\t\t\t    |     |");
+                                        System.out.println("\t\t\t   _|_");
+                            break;
+                            case 8:	System.out.println("\t\t\t    /¯¯¯¯¯|");
+                                        System.out.println("\t\t\t    |    _o_");
+                                        System.out.println("\t\t\t    |    _|_");
+                                        System.out.println("\t\t\t   _|_");
+                                        System.out.println("\nLo siento. "
+                                                + "Has perdido el juego :(");
+                        }
+                }System.out.println();
+        } while(errores<8 && !sw);
+        
+        System.out.println("___________________________________\n");
+        System.out.println("La palabra a adivinar era: '" + eleccion + "'");
+        System.out.println("Nº Intentos: " + (letrasUsadas.length()-1) +
+                           ".\tDuración de Partida: "
+                           + (new Date().getTime()-ti) / 1000 + " segundos");
+    }
+}
